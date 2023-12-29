051c4dce4ddbf118c2d300a2dc06b6ca81214a56
Anas Munir <anas@ovou.com>
2023-12-30 00:26:00 +0500

    removed unused code in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,16 @@
-import { Button } from '@chakra-ui/react';
-import { Link, Route, Routes } from 'react-router-dom';
-import Home from './pages/home/home';
-import Product from './pages/product/product';
-import Checkout from './pages/checkout/checkout';
+import { Route, Routes } from 'react-router-dom';
+
+import Home from './pages/home/Home';
+import Product from './pages/product/Product';
+import Checkout from './pages/checkout/Checkout';
 
 function App() {
   return (
-    <>
-      <Button variant='primary'>
-        <Link to='/'>Home</Link>
-      </Button>
-      <Button variant='primary'>
-        <Link to='/product/salad'>Product</Link>
-      </Button>
-      <Button variant='primary'>
-        <Link to='/checkout'>Checkout</Link>
-      </Button>
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/product/:id' element={<Product />}></Route>
-        <Route path='/checkout' element={<Checkout />}></Route>
-      </Routes>
-    </>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/product/:id' element={<Product />}></Route>
+      <Route path='/checkout' element={<Checkout />}></Route>
+    </Routes>
   );
 }
 

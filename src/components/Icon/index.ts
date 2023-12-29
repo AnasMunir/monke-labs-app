d2cb4ec4ae5903de167c14c7b4f9246e57bfb0f2
Anas Munir <anas@ovou.com>
2023-12-30 00:02:05 +0500

    added custom svg icons

---
+++ src/components/Icon/index.ts
@@ -0,0 +1,5 @@
+export { default as CartIcon } from './svg/CartIcon';
+export { default as ChartIcon } from './svg/ChartIcon';
+export { default as HomeIcon } from './svg/HomeIcon';
+export { default as ProfileIcon } from './svg/ProfileIcon';
+export { default as HeartIcon } from './svg/HeartIcon';

c09711b57ac8d328fad0cbf4571223e3f593b3eb
Anas Munir <anas@ovou.com>
2023-12-29 18:52:08 +0500

    added some routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import { useState } from 'react';
-import './App.css';
 import { Button } from '@chakra-ui/react';
+import { Link, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/home';
+import Product from './pages/product/product';
+import Checkout from './pages/checkout/checkout';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      {/* <Button bg='brand.primary.500' color='white' fontWeight={600}> */}
-      <Button variant='primary'>Add to cart</Button>
+      <Button variant='primary'>
+        <Link to='/'>Home</Link>
+      </Button>
+      <Button variant='primary'>
+        <Link to='/product/salad'>Product</Link>
+      </Button>
+      <Button variant='primary'>
+        <Link to='/checkout'>Checkout</Link>
+      </Button>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/product/:id' element={<Product />}></Route>
+        <Route path='/checkout' element={<Checkout />}></Route>
+      </Routes>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,14 @@ import './index.css';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import theme from './theme';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );

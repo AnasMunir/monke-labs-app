85d2bd1435766a9a568fbd89ccf620c9ee92a517
Anas Munir <anas@ovou.com>
2023-12-30 15:18:39 +0500

    created order now feature

---
+++ src/features/index.ts
@@ -0,0 +1,1 @@
+export { default as OrderNow } from './order-now/OrderNow';
---
+++ src/features/order-now/OrderNow.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import ProductItem from '../../components/product-item/ProductItem';
+
+const OrderNow: FC = () => {
+  return <ProductItem />;
+};
+
+export default OrderNow;

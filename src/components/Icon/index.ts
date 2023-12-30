db4e70daa2d5c43cf159f8e85f3ebc29e19abb88
Anas Munir <anas@ovou.com>
2023-12-30 21:36:49 +0500

    created search icon

--- src/components/Icon/index.ts
+++ src/components/Icon/index.ts
@@ -3,3 +3,4 @@ export { default as ChartIcon } from './svg/ChartIcon';
 export { default as HomeIcon } from './svg/HomeIcon';
 export { default as ProfileIcon } from './svg/ProfileIcon';
 export { default as HeartIcon } from './svg/HeartIcon';
+export { default as SearchIcon } from './svg/SearchIcon';

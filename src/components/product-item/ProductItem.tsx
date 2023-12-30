5daa8a13ea4145f9d71a25cc2a37300656b213a0
Anas Munir <anas@ovou.com>
2023-12-30 17:16:07 +0500

    adjusted product item

--- src/components/product-item/ProductItem.tsx
+++ src/components/product-item/ProductItem.tsx
@@ -7,9 +7,13 @@ import Price from '../price/Price';
 
 const ProductItem: FC = () => {
   return (
-    <Card maxW='sm' w='172px' height='85px' borderRadius='20px'>
-      <CardBody p='0px' bg='white' borderRadius='inherit' filter='drop-shadow(0px 2px 20px rgba(0, 0, 0, 0.05))'>
-        <Image src={RectangleSix} alt='Green double couch with wooden legs' />
+    <Card borderRadius='20px'>
+      <CardBody
+        padding='0px'
+        background='white'
+        borderRadius='inherit'
+        filter='drop-shadow(0px 2px 20px rgba(0, 0, 0, 0.05))'>
+        <Image width='100%' src={RectangleSix} alt='Green double couch with wooden legs' />
         <Stack spacing='2' px='13px' py='8px'>
           <Text fontSize='14px' fontWeight={500} color='black'>
             Green Salas with mashed potatoes

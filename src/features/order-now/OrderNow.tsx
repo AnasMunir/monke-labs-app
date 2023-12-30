e7fcad362fcd835d062502f69bc32818139a324b
Anas Munir <anas@ovou.com>
2023-12-30 17:16:40 +0500

    added category to ordernow page

--- src/features/order-now/OrderNow.tsx
+++ src/features/order-now/OrderNow.tsx
@@ -1,8 +1,29 @@
 import { FC } from 'react';
 import ProductItem from '../../components/product-item/ProductItem';
+import { Box, HStack } from '@chakra-ui/react';
+import CategoryItem from '../../components/category-item/CategoryItem';
 
 const OrderNow: FC = () => {
-  return <ProductItem />;
+  return (
+    <>
+      <HStack
+        overflow='auto'
+        whiteSpace='nowrap'
+        sx={{
+          '::-webkit-scrollbar': {
+            display: 'none',
+          },
+        }}>
+        {[...Array(12)].map((_, index) => (
+          <CategoryItem key={index} />
+        ))}
+      </HStack>
+      <Box marginBottom={10} />
+      <Box width={{ base: '172px', md: '200px' }}>
+        <ProductItem />
+      </Box>
+    </>
+  );
 };
 
 export default OrderNow;

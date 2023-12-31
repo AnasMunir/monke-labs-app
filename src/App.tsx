c7654bcfa19d54e54d0b37ab3a223cd4037f65bd
Anas Munir <anas@ovou.com>
2023-12-31 14:25:51 +0500

    removed product page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 
 import Home from './pages/home/Home';
-import Product from './pages/product/Product';
 import Checkout from './pages/checkout/Checkout';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />}></Route>
-      <Route path='/product/:id' element={<Product />}></Route>
       <Route path='/checkout' element={<Checkout />}></Route>
     </Routes>
   );

72efe9fda846cc297774e067ff6553be467afd1d
Anas Munir <anas@ovou.com>
2023-12-31 09:37:59 +0500

    fixed price component

--- src/components/price/Price.tsx
+++ src/components/price/Price.tsx
@@ -1,4 +1,4 @@
-import { HStack, Text, TextProps } from '@chakra-ui/react';
+import { Flex, HStack, Text, TextProps } from '@chakra-ui/react';
 import { FC } from 'react';
 
 interface IPrice extends TextProps {
@@ -7,12 +7,12 @@ interface IPrice extends TextProps {
 }
 const Price: FC<IPrice> = ({ reverse = false, price, ...props }) => {
   return (
-    <HStack direction={reverse ? 'row-reverse' : 'row'} spacing='1'>
+    <Flex flexDirection={reverse ? 'row-reverse' : 'row'}>
       <Text {...props} color='brand.primary.500'>
         $
       </Text>
       <Text {...props}>{price}</Text>
-    </HStack>
+    </Flex>
   );
 };
 

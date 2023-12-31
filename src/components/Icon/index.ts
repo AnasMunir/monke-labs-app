4c4693ea34b105dd8467258bf36f895a292ca049
Anas Munir <anas@ovou.com>
2023-12-31 09:37:35 +0500

    added new icons from figma

--- src/components/Icon/index.ts
+++ src/components/Icon/index.ts
@@ -4,3 +4,6 @@ export { default as HomeIcon } from './svg/HomeIcon';
 export { default as ProfileIcon } from './svg/ProfileIcon';
 export { default as HeartIcon } from './svg/HeartIcon';
 export { default as SearchIcon } from './svg/SearchIcon';
+export { default as BackArrowIcon } from './svg/BackArrowIcon';
+export { default as PlusIcon } from './svg/PlusIcon';
+export { default as MinusIcon } from './svg/MinusIcon';
---
+++ src/components/Icon/svg/BackArrowIcon.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react';
+
+const BackArrowIcon: FC = () => {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' width='40' height='40' viewBox='0 0 40 40' fill='none'>
+      <path
+        d='M15.95 9.88333L5.83331 20L15.95 30.1167M34.1666 20H6.11665'
+        stroke='black'
+        strokeWidth='1.5'
+        strokeMiterlimit='10'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+};
+
+export default BackArrowIcon;
